6a10a786a61b2f01bced99db545560871a06fa17
Geoffrey Mainland <mainland@apeiron.net>
2021-03-30 09:26:11 -0400

    Add solutions from lecture.

--- week1/scheme-examples.rkt
+++ week1/scheme-examples.rkt
@@ -18,7 +18,9 @@
 ;
 
 (define (length xs)
-  'not-implemented)
+  (if (null? xs)
+      0
+      (+ 1 (length (rest xs)))))
 
 ;
 ; Compute the sum of a list of integers
@@ -27,7 +29,9 @@
 ;   (sum '(1 2 3 4)) => 10
 
 (define (sum xs)
-  'not-implemented)
+  (if (null? xs)
+      0
+      (+ (first xs) (sum (rest xs)))))
 
 ; Return the nth element of a list, counting from 0.
 ;
@@ -36,7 +40,9 @@
 ;  (nth 2 '(1 2 3)) => 3
 
 (define (nth n xs)
-  'not-implemented)
+  (if (= n 0)
+      (first xs)
+      (nth (- n 1) (rest xs))))
 
 ; Concatenate the lists l1 and l2 (append l2 to l1)
 ;   The concatention of l1 and l2 is equal to l2 if l1 is null.
@@ -49,7 +55,10 @@
 ;
 
 (define (concat xs ys)
-  'not-implemented)
+  (if (null? xs)
+      ys
+      (cons (first xs) (concat (rest xs) ys))))
+      
 
 ; Count the number of integers in a list
 ;  The number of integers in an object that is an integer is 1.
@@ -77,7 +86,9 @@
 ;  (inc-all '(1 2 3 4 5)) => '(2 3 4 5 6)
 
 (define (inc-all xs)
-  'not-implemented)
+  (if (null? xs)
+      null
+      (cons (+ 1 (first xs)) (inc-all (rest xs)))))
   
 ; Return a list containing all the elements of xs incremented by n
 ;
@@ -87,7 +98,9 @@
 ;   (add-n-all 1 '(1 2 3 4 5)) => '(2 3 4 5 6)
 
 (define (add-n-all n xs)
-  'not-implemented)
+  (if (null? xs)
+      null
+      (cons (+ n (first xs)) (add-n-all n (rest xs)))))
 
 ; Return a list containing all the elements of xs when passed as arguments to a
 ; function.
@@ -99,7 +112,9 @@
 ;
 
 (define (apply-all f xs)
-  'not-implemented)
+  (if (null? xs)
+      null
+      (cons (f (first xs)) (apply-all f (rest xs)))))
 
 ;
 ; Calculate the squares of a list of integers. Make the function non-recursive.
@@ -109,7 +124,7 @@
 ;
 
 (define (squares xs)
-  'not-implemented)
+  (map (lambda (x) (* x x)) xs))
 
 ;
 ; Return a list containing only the elements of xs for which the predicate f
@@ -120,7 +135,9 @@
 ;
 
 (define (filter f xs)
-  'not-implemented)
+  (cond [(null? xs)     null]
+        [(f (first xs)) (cons (first xs) (filter f (rest xs)))]
+        [else           (filter f (rest xs))]))
 
 ;
 ; Write a non-recursive function that takes a list and returns all the even
@@ -130,25 +147,25 @@
 ;   (only-even '(1 2 3 4 5)) => '(2 4)
 
 (define (only-even xs)
-  'not-implemented)
+  (filter even? xs))
 
 ; Write a function that composes two functions.
 ; That is, ((compose f g) x) should be the same as (f (g x))
 
 (define (compose f g)
-  'not-implemented)
+  (lambda (x) (f (g x))))
 
 ; Write a function that partially applies a function to a single argument.
 ; That is, ((papply f x) y) should be the same as (f x y)
 
 (define (papply f x)
-  'not-implemented)
+  (lambda (y) (f x y)))
 
 ; Add one to a number
 ; Define using papply
 
 (define inc
-  'not-implemented)
+  (papply + 1))
 
 ;;
 ;; Folds
@@ -157,13 +174,17 @@
 ; Right fold
 
 (define (foldr f z xs)
-  'not-implemented)
+  (if (null? xs)
+      z
+      (f (first xs) (foldr f z (rest xs)))))
 
 ;; (show-tree (foldr (show-function 'cons) 'null '(1 2 3 4)))
 
 ; Left fold
 
 (define (foldl f z xs)
-  'not-implemented)
+  (if (null? xs)
+      z
+      (foldl f (f (first xs) z) (rest xs))))
 
 ;; (show-tree (foldl (show-function 'cons) 'null '(1 2 3 4)))
